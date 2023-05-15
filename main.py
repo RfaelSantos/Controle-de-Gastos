c06fdd102ee0b34120d1a9500a755f27f21fe985
Rafael Antunes <rafael-1-8@live.com>
2023-05-15 20:42:00 -0300

    Incluindo a opção de exclusão

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-import tkinter
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import pyodbc
-import pandas as pd
 
 Server = 'LAPTOP-0O4DDNV9\SQLEXPRESS'
 conexao = pyodbc.connect(Driver='{SQL Server}', host=Server)
 gastos = conexao.execute("SELECT * FROM [Controle_Gastos].[dbo].[Gasto]")
-print(gastos)
+
 
 '''Tela principal'''
+
+def delete():
+
+    confirmacao = messagebox.askokcancel(title="Confirmação", message="Tem certeza que deseja apagar o registro?")
+
+    if confirmacao:
+        conexao.execute("DELETE FROM [Controle_Gastos].[dbo].[Gasto] WHERE id = '10' ")
+        conexao.commit()
+
+        messagebox.showinfo(message="Registro deletado com sucesso!")
+
+
 window = Tk()
 window.title('Controle de Gastos')
 window.geometry("400x400")
@@ -21,7 +31,7 @@ add_button.grid(column=1, row=1)
 alt_button = Button(text='Alterar', width=10)
 alt_button.grid(column=1, row=2)
 
-del_button = Button(text='Excluir', width=10)
+del_button = Button(text='Excluir', width=10, command=delete)
 del_button.grid(column=1, row=3)
 
 tree = ttk.Treeview(window, selectmode="browse", columns=('column1', 'column2', 'column3'), show="headings")
